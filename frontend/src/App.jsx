a707b647009916c4d894568f5259221e79dcb67a
Zp1021 <90364004+Zp1021@users.noreply.github.com>
2026-03-16 21:00:02 -0400

    Added react-router-dom dependency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
